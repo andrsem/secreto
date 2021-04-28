f289d2ca7abca4c8c6930da3c1701e9739d13ffe
andrsem <andriisem0@gmail.com>
2021-04-28 10:58:40 +0300

    create constants for LED and buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,30 @@
 #include <Arduino.h>
+
+const int pin1 = 12;
+const int pin2 = 11;
+const int pin3 = 10;
+const int pin4 = 9;
+const int pin5 = 8;
+const int pin6 = 7;
+const int pin7 = 6;
+const int pin8 = 5;
+const int pinKeyLeft = 3;
+const int pinKeyRight = 2;
+
+void setup()
+{
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  pinMode(pin5, OUTPUT);
+  pinMode(pin6, OUTPUT);
+  pinMode(pin7, OUTPUT);
+  pinMode(pin8, OUTPUT);
+  pinMode(pinKeyLeft, INPUT_PULLUP);
+  pinMode(pinKeyRight, INPUT_PULLUP);
+}
+
+void loop()
+{
+}
